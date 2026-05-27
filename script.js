32ba587fe888c19df4926d2ba20b0f221cabdad6
alicesorbara-stack <alice.sorbara@escola.pr.gov.br>
2026-05-27 11:59:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-// Arquivo preparado para futuras animações
-console.log("Projeto Agrinho carregado com sucesso!");
+const accordions =
+document.querySelectorAll(".accordion");
+
+accordions.forEach((accordion) => {
+
+accordion.addEventListener("click", () => {
+
+accordion.classList.toggle("ativo");
+
+const painel =
+accordion.nextElementSibling;
+
+if(painel.style.maxHeight){
+
+painel.style.maxHeight = null;
+
+}else{
+
+painel.style.maxHeight =
+painel.scrollHeight + "px";
+
+}
+
+});
+
+});
